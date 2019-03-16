5d6ec9ea89bc5c0cbe1aa9d7f6caae015a9d8f3f
Sinjon Santos <sinjon@berkeley.edu>
2019-03-16 12:52:27 -0700

    Files were excluded

--- SnapchatClone/AppDelegate.swift
+++ SnapchatClone/AppDelegate.swift
@@ -24,17 +24,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillResignActive(_ application: UIApplication) {
         /* PART 1 START */
+        
         let center =  UNUserNotificationCenter.current()
-        let content = UNMutableNotificationContent()
-        content.title = "Hey Bitch"
-        content.subtitle = "Open me"
-        content.body = "i hate coding"
-        content.sound = UNNotificationSound.default()
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval:5.0, repeats: false)
-        let request = UNNotificationRequest(identifier: "ContentIdentifier", content: content, trigger: trigger)
-        center.add(request) { (error) in
-            if error != nil {
-                print("error \(String(describing: error))")
+        center.requestAuthorization(options: [.alert, .sound])
+        { (granted, error) in
+            let content = UNMutableNotificationContent()
+            content.title = "Hey Bitch"
+            content.subtitle = "Open me"
+            content.body = "i hate coding"
+            content.sound = UNNotificationSound.default()
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval:5.0, repeats: false)
+            let request = UNNotificationRequest(identifier: "ContentIdentifier", content: content, trigger: trigger)
+            center.add(request) { (error) in
+                if error != nil {
+                    print("error \(String(describing: error))")
+                }
             }
         }
 
